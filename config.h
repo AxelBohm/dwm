b5c14980df9187e8afb1e3de36bce47c742407b4
AxelBohm <axel.boehm@ymail.com>
2018-09-20 18:12:10 +0200

    couple key changes

--- config.h
+++ config.h
@@ -61,16 +61,25 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[]   = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]    = { "st",                                       NULL };
+/* screen */
+static const char *screensetup[]= { "monitor.sh",                               NULL };
+static const char *screenext[]  = { "set-external.sh",                          NULL };
+/* sound */
 static const char *volup[]      = { "amixer", "sset", "Master", "5%+",          NULL };
 static const char *voldown[]    = { "amixer", "sset", "Master", "5%-",          NULL };
 static const char *volmute[]    = { "amixer", "sset", "Master", "toggle",       NULL };
+/* screen brightness */
 static const char *bklu[]       = { "xbacklight", "-inc", "5",                  NULL };
 static const char *bkld[]       = { "xbacklight", "-dec", "5",                  NULL };
+/* music */
+static const char *mpcnext[]    = { "mpc", "next",                              NULL };
+static const char *mpcprev[]    = { "mpc", "prev",                              NULL };
+static const char *mpctoggle[]  = { "mpc", "toggle",                            NULL };
+/* misc */
 static const char *lockcmd[]    = { "slock",                                    NULL };
 static const char *mailcmd[]    = { "st", "-e", "neomutt",                      NULL };
 static const char *browsercmd[] = { "firefox",                                  NULL };
 static const char *roficmd[]    = { "rofi", "-combi-modi", "window,drun", "-show combi", NULL};
-
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -80,6 +89,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_b,      spawn,          {.v = browsercmd } },
 	{ ALTKEY,                       XK_space,  spawn,          {.v = roficmd } },
 	{ MODKEY,                       XK_Delete, spawn,          {.v = lockcmd } },
+	{ MODKEY|ShiftMask,             XK_s,      spawn,          {.v = screensetup } },
+    /* base */
 	{ MODKEY|ShiftMask,             XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -102,6 +113,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+    /* tagkey */
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
@@ -117,8 +129,14 @@ static Key keys[] = {
 	{ 0,                XF86XK_AudioMute,             spawn,   {.v = volmute } },
 	{ 0,                XF86XK_AudioRaiseVolume,      spawn,   {.v = volup   } },
     /* screen brightness */
-    { 0,                 XF86XK_MonBrightnessUp,       spawn,   {.v = bklu } },
-    { 0,                 XF86XK_MonBrightnessDown,     spawn,   {.v = bkld } },
+    { 0,                XF86XK_MonBrightnessUp,       spawn,   {.v = bklu } },
+    { 0,                XF86XK_MonBrightnessDown,     spawn,   {.v = bkld } },
+    /* screen setup */
+	{ 0,                XF86XK_Display,               spawn,   {.v = screenext } },
+    /* music */
+	{ 0,                XF86XK_AudioPlay,             spawn,   {.v = mpctoggle } },
+	{ 0,                XF86XK_AudioNext,             spawn,   {.v = mpcnext } },
+	{ 0,                XF86XK_AudioPrev,             spawn,   {.v = mpcprev } },
 };
 
 /* button definitions */
