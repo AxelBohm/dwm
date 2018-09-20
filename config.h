695d2cc95fca87b3f636a1c9ba9403dd37e31684
AxelBohm <axel.boehm@ymail.com>
2018-09-20 12:35:27 +0200

    whitespace

--- config.h
+++ config.h
@@ -58,16 +58,17 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
-static const char *volup[]     = {"amixer", "sset", "Master", "5%+",      NULL};
-static const char *voldown[] = {"amixer", "sset", "Master", "5%-", NULL};
-static const char *volmute[]   = {"amixer", "sset", "Master", "toggle",   NULL};
-static const char *bklu[] = { "xbacklight", "-inc", "5", NULL };
-static const char *bkld[] = { "xbacklight", "-dec", "5", NULL };
-static const char *mailcmd[] = { "st", "-e", "neomutt", NULL };
-static const char *browsercmd[] = { "firefox", NULL };
-static const char *roficmd[] = {"rofi -combi-modi window,drun -show combi", NULL};
+static const char *dmenucmd[]   = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *termcmd[]    = { "st",                                       NULL };
+static const char *volup[]      = { "amixer", "sset", "Master", "5%+",          NULL };
+static const char *voldown[]    = { "amixer", "sset", "Master", "5%-",          NULL };
+static const char *volmute[]    = { "amixer", "sset", "Master", "toggle",       NULL };
+static const char *bklu[]       = { "xbacklight", "-inc", "5",                  NULL };
+static const char *bkld[]       = { "xbacklight", "-dec", "5",                  NULL };
+static const char *lockcmd[]    = { "slock",                                    NULL };
+static const char *mailcmd[]    = { "st", "-e", "neomutt",                      NULL };
+static const char *browsercmd[] = { "firefox",                                  NULL };
+static const char *roficmd[]    = { "rofi", "-combi-modi", "window,drun", "-show combi", NULL};
 
 
 static Key keys[] = {
@@ -77,6 +78,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_m,      spawn,          {.v = mailcmd } },
 	{ MODKEY,                       XK_b,      spawn,          {.v = browsercmd } },
 	{ ALTKEY,                       XK_space,  spawn,          {.v = roficmd } },
+	{ MODKEY,                       XK_Delete, spawn,          {.v = lockcmd } },
 	{ MODKEY|ShiftMask,             XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
