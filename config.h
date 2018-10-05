19c1d81cf2fab90ba8362502784f87afb12e3524
AxelBohm <axel.boehm@ymail.com>
2018-10-05 14:38:03 +0200

    fixed name of chat app

--- config.h
+++ config.h
@@ -41,7 +41,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Firefox",  NULL,       NULL,       1 << 1,       0,           -1 },
 	{ "mutt",     NULL,       NULL,       1 << 0,       0,           -1 },
-	{ "rambox",   NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Rambox",   NULL,       NULL,       1 << 6,       0,           -1 },
 };
 
 /* layout(s) */
