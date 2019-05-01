7bc7439bf06f8fea5ddb7f0372eac07979832a8a
AxelBohm <axel.boehm@ymail.com>
2019-05-01 09:55:20 -0400

    icons for workspaces

--- config.h
+++ config.h
@@ -28,7 +28,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "7", "8", "9" };
+static const char *tags[] = { "", "", "", "4", "7", "8", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
