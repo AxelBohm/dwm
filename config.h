c2f46e794db930f64842f47c5379505e267350d4
AxelBohm <axel.boehm@ymail.com>
2019-09-25 10:55:53 -0500

    in order to make super+control work

--- config.h
+++ config.h
@@ -67,7 +67,7 @@ static const Layout layouts[] = {
 #define ALTKEY Mod1Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask,           KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
